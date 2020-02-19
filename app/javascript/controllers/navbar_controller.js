fc45a044d0cf35ee90b3667224352f031213273a
HermannHH <harris.hermann@gmail.com>
2020-02-19 23:13:10 +0200

    Updates

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -30,41 +30,8 @@ export default class extends Controller {
     }
   }
 
-  bottomOfWindowAdjustments = () => {
-    this.element.classList.remove('bottom-0');
-    this.element.classList.add('top-0');
-
-    this.element.classList.remove('absolute');
-    this.element.classList.add('fixed');
-
-    this.element.classList.remove('opacity-25');
-    this.element.classList.add('opacity-100');
-
-    this.element.classList.remove('bg-gray-800');
-    this.element.classList.add('bg-white');
-  }
-
-  topOfWindowAdjustments = () => {
-    this.element.classList.remove('top-0');
-    this.element.classList.add('bottom-0');
-
-    this.element.classList.remove('fixed');
-    this.element.classList.add('absolute');
-
-    this.element.classList.remove('opacity-100');
-    this.element.classList.add('opacity-25');
-
-    this.element.classList.remove('bg-white');
-    this.element.classList.add('bg-gray-800');
-  }
-
   scrollToSection = (event) => {
     event.preventDefault();
-    // console.log('document.querySelector(event.srcElement.dataset.targetHash)', event.srcElement.dataset.targetHash, )
-    // document.querySelector(event.srcElement.dataset.targetHash).scrollIntoView({
-    //   top: -220,
-    //   behavior: 'smooth'
-    // });
     var fromTop = document.querySelector(event.srcElement.dataset.targetHash).offsetTop;
     window.scroll({ top: fromTop - 85, left: 0, behavior: 'smooth' });
   }
