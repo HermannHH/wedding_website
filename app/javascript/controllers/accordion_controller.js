61a159ceca6566f89c9e5dd7b1cd843e4edee6c7
HermannHH <harris.hermann@gmail.com>
2020-02-22 13:22:42 +0200

    Animated accordion

--- app/javascript/controllers/accordion_controller.js
+++ app/javascript/controllers/accordion_controller.js
@@ -7,44 +7,18 @@ export default class extends Controller {
     // this.setupExpandFunctionality();
   }
 
-  setupExpandFunctionality = () => {
-    var acc = this.element;
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-          panel.style.maxHeight = null;
-        } else {
-          panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-      });
-    }
-  }
-
   toggleItem(event) {
+
+    // console.log('this.itemTargets', this.itemTargets)
     const panel = event.currentTarget.querySelector('.accordion-panel');
-    // TODO: Close all item targets
+
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
+      this.itemTargets.forEach(element => {
+        element.querySelector('.accordion-panel').style.maxHeight = null;
+      });
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
-    //  if(panel.classList.contains('active')) {
-    //   panel.classList.remove('h-0');
-    //   panel.classList.add('h-auto');
-    // } else {
-    //   panel.classList.add('h-0');
-    //   panel.classList.remove('h-auto');
-    // }
-    // if(panel.classList.contains('h-0')) {
-    //   panel.classList.remove('h-0');
-    //   panel.classList.add('h-auto');
-    // } else {
-    //   panel.classList.add('h-0');
-    //   panel.classList.remove('h-auto');
-    // }
   }
 }
