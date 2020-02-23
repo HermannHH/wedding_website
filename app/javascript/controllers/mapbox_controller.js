6ced7a53860cd9fb2ed8bbfa34c72564d2b39869
HermannHH <harris.hermann@gmail.com>
2020-02-23 15:09:00 +0200

    Update mapbox_controller.js

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -16,11 +16,26 @@ export default class extends Controller {
   connect() {
     window.addEventListener("resize", this.handleVideoDimensionsSet);
     this.handleVideoDimensionsSet();
+    var latLng = [-34.036053, 18.801706];
+    var html = `
+      <div>
+        <a target="_blank" href="https://goo.gl/maps/2gkZbN7ndL2JsWtH9" class="bg-gray-300 w-full my-2 font-bold py-2 px-4 rounded flex justify-center items-center">
+          Open in Google Maps
+        </a>
+        <a target="_blank" href="https://m.uber.com/ul/?action=setPickup&client_id=ycyh-QdfEzc3PDM8id7OO08z0n-tYAr-&pickup=my_location&dropoff[formatted_address]=Winery%20Road%20Forest%2C%20Winery%20Road%2C%20Firgrove%2C%20South%20Africa&dropoff[latitude]=-34.037031&dropoff[longitude]=18.801770" class="bg-gray-300 w-full my-2 font-bold py-2 px-4 rounded flex justify-center items-center">
+          Book an Uber
+        </a>
+
+    `;
     L.mapbox.accessToken = 'pk.eyJ1IjoiaGVybWFubmhoIiwiYSI6ImNqdjhjMm1heTBlOXU0NG55c3ZqbmlvaTMifQ.TJXMyMhgTFQ_HIeiyZsIzA';
     var map = L.mapbox.map(this.element)
-    .setView([-34.036053, 18.801706], 13)
+    .setView(latLng, 13)
     .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
-    L.marker([-34.036053, 18.801706]).addTo(map);
+    L.marker(latLng).addTo(map);
+    var popup = L.popup({ closeButton: false })
+    .setLatLng(latLng)
+    .setContent(html)
+    .openOn(map);
     if (map.scrollWheelZoom) {
       map.scrollWheelZoom.disable();
     }
