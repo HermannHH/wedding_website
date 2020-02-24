2e953305b9c6302c402a5d2aca8fbd106b46c18c
HermannHH <harris.hermann@gmail.com>
2020-02-24 09:59:43 +0200

    Rolling css gallery

--- app/javascript/apps/gallery/index.jsx
+++ app/javascript/apps/gallery/index.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import Swiper from 'react-id-swiper';
 
 function Gallery({ data }) {
-  console.log('data', data)
   const params = {
 
     containerClass: 'polaroid-images', // Replace swiper-container with customized-swiper-container
     slidesPerView: 5,
     loop: true,
-    fadeEffect: {
-      crossFade: true
-    },
+    // fadeEffect: {
+    //   crossFade: true
+    // },
     autoplay: {
       delay: 3000
     }
