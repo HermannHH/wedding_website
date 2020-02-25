3f7564282817f7e68d0f4182c4ad7460ca213144
HermannHH <harris.hermann@gmail.com>
2020-02-25 07:16:56 +0200

    Deploy updates

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("css/application.scss")
+require("../stylesheets/base");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,32 +17,3 @@ require("css/application.scss")
 // const imagePath = (name) => images(name, true)
 
 import "controllers";
-import Glide, { Controls, Breakpoints, Swipe } from '@glidejs/glide/dist/glide.modular.esm';
-
-
-
-// document.addEventListener("turbolinks:load", function() {
-//   new Glide('.glide').mount({ Controls, Breakpoints, Swipe });
-// });
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Carousel from '../apps/carousel';
-
-// document.addEventListener("turbolinks:load", function() {
-//   var element = document.getElementById('tester');
-//   console.log('tester', tester)
-//   if(typeof(element) != 'undefined' && element != null){
-//     ReactDOM.render(
-//       <Carousel />,
-//       element.appendChild(document.createElement('div')),
-//     )
-//   }
-// });
-
-import axios from 'axios';
-
-
-document.addEventListener("turbolinks:load", function() {
-  const csrfToken = document.querySelector('[name=csrf-token]').content;
-  axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
-});
