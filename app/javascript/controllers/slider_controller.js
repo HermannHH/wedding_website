c71537f8cec7ee134fdda93df78806f16fcd61da
HermannHH <harris.hermann@gmail.com>
2020-02-29 10:31:56 +0200

    WIP: Banner

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,42 +1,48 @@
-// Visit The Stimulus Handbook for more details
-// https://stimulusjs.org/handbook/introduction
-//
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
+import { Controller } from "stimulus";
 
-import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "slideItem", "slideTrack" ]
+  static targets = [ "track", "item" ]
 
   connect() {
-    // this.outputTarget.textContent = 'Hello, Stimulus!'
-    console.log('swiper loaded', this.slideItemTargets.length);
-      // this.toInfinityAndBeyond();
-      new Swiper (this.element, {
-        effect: 'fade',
-        autoplay: {
-          delay: 5000,
-        },
-      })
+    this.initSmoothScrolling();
   }
 
-  // toInfinityAndBeyond = () => {
-  //   // console.log('this.slideTrackTarget', this.slideTrackTarget)
-  //   setInterval(() => {
-  //     this.slideItemTargets.forEach(element => {
-  //       var bounds = element.getBoundingClientRect();
-  //       console.log('bounds', element, bounds.left)
-  //       if(bounds.left < 0) {
-  //         console.log('i am smoooolllll', element)
-  //         this.slideTrackTarget.appendChild(element);
-  //         // element.remove();
-  //       }
-  //     });
+  initSmoothScrolling = () => {
+    var sliderWidth = 0;
+    var animationWidth = 0;
+    var sliderHeight = this.trackTarget.offsetHeight;
+    // console.log('init', this.outerHeight(this.trackTarget));
 
-  //   }, 3000);
-  // }
+
+    this.itemTargets.forEach(el => {
+      console.log('tt', el)
+      animationWidth += el.offsetWidth;
+
+      var slidesVisible = this.element.offsetWidth / this.itemTargets[0].offsetWidth;
+      slidesVisible = Math.ceil(slidesVisible);
+
+      // count slides to determine animation speed
+      var slidesNumber = this.itemTargets.length;
+      var speed = slidesNumber * 2;
+
+      // append the tail
+      // el.slice(0,slidesVisible).clone().appendTo(this.trackTarget);
+
+      // Detect the slider width with appended tail
+      // $('>div>div', container).each(function(){
+      //   sliderWidth += $(this).outerWidth(false);
+      // });
+
+    });
+  }
+
+  outerHeight = (element) => {
+    const height = element.offsetHeight,
+        style = window.getComputedStyle(element)
+
+    return ['top', 'bottom']
+        .map(side => parseInt(style[`margin-${side}`]))
+        .reduce((total, side) => total + side, height)
+  }
 }
