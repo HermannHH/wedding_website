0c0ddfc833f68f4e7e79f6a41a79cade664541a1
HermannHH <harris.hermann@gmail.com>
2020-02-29 13:45:49 +0200

    Slider working

---
+++ app/javascript/controllers/ticker_controller.js
@@ -0,0 +1,32 @@
+// Visit The Stimulus Handbook for more details
+// https://stimulusjs.org/handbook/introduction
+//
+// This example controller works with specially annotated HTML like:
+//
+// <div data-controller="hello">
+//   <h1 data-target="hello.output"></h1>
+// </div>
+
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "track", "slide", "item" ]
+
+  connect() {
+    console.log('hello')
+  }
+
+
+  mouseOver(event) {
+    this.slideTargets.forEach(element => {
+      element.classList.add('paused');
+    });
+  }
+
+
+  mouseOut(event) {
+    this.slideTargets.forEach(element => {
+      element.classList.remove('paused');
+    });
+  }
+}
