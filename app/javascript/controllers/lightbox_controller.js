31280da9800c657cba56bf695afd970a396c8ac6
HermannHH <harris.hermann@gmail.com>
2020-03-01 16:46:20 +0200

    Update lightbox_controller.js

--- app/javascript/controllers/lightbox_controller.js
+++ app/javascript/controllers/lightbox_controller.js
@@ -1,21 +1,21 @@
-import { Controller } from "stimulus";
-import PhotoSwipe from "photoswipe/dist/photoswipe.min.js";
-import PhotoSwipeUI_Default from "photoswipe/dist/photoswipe-ui-default.min.js";
+// import { Controller } from "stimulus";
+// import PhotoSwipe from "photoswipe/dist/photoswipe.min.js";
+// import PhotoSwipeUI_Default from "photoswipe/dist/photoswipe-ui-default.min.js";
 
-export default class extends Controller {
+// export default class extends Controller {
 
-  connect() {
-  }
+//   connect() {
+//   }
 
-  openGallery() {
-    var pswpElement = document.querySelectorAll('.pswp')[0];
-    var items = JSON.parse(this.data.get("images")).map( item => ({ src: item, w: 600, h: 400 }));
-    console.log('items', items)
-    var options = {
-        index: 0
-    };
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-    gallery.init();
-  }
+//   openGallery() {
+//     var pswpElement = document.querySelectorAll('.pswp')[0];
+//     var items = JSON.parse(this.data.get("images")).map( item => ({ src: item, w: 600, h: 400 }));
+//     console.log('items', items)
+//     var options = {
+//         index: 0
+//     };
+//     var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+//     gallery.init();
+//   }
 
-}
+// }
