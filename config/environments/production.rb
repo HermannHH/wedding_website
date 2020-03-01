be0b3569dbbcc9f5188f2938c4433cb1b5802616
HermannHH <harris.hermann@gmail.com>
2020-03-01 16:27:42 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,10 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = { host: 'https://hermanth.com'  }
+
 end
+
+
+Rails.application.default_url_options = Rails.application.config.action_mailer.default_url_options
