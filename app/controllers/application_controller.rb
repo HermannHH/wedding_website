b9527e623ff17af0f5279d864b864ebc08b8ca90
HermannHH <harris.hermann@gmail.com>
2020-03-01 17:20:52 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,14 @@ class ApplicationController < ActionController::Base
 
   include Schemas
 
+  private
 
+  def after_sign_in_path_for(resource)
+    authenticated_root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 
 end
