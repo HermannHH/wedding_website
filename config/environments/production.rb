65fb4041b164f0b0d47e88bebefc436c07d14b84
HermannHH <harris.hermann@gmail.com>
2020-03-03 21:34:20 +0200

    Updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,16 +113,16 @@ Rails.application.configure do
   config.action_controller.default_url_options = { host: 'https://hermantha.com' }
   config.action_mailer.default_url_options = { host: 'https://hermantha.com'  }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              "smtp.sendgrid.net",
-    port:                 "587",
-    authentication:       :plain,
-    user_name:            ENV['SENDGRID_USERNAME'],
-    password:             ENV['SENDGRID_PASSWORD'],
-    # domain:               'hermantha.com',
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              "smtp.sendgrid.net",
+  #   port:                 "587",
+  #   authentication:       :plain,
+  #   user_name:            ENV['SENDGRID_USERNAME'],
+  #   password:             ENV['SENDGRID_PASSWORD'],
+  #   # domain:               'hermantha.com',
+  #   enable_starttls_auto: true
+  # }
 
 end
 
