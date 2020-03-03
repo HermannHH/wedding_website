87919a100bd7eb940a131ae869b38c33db615b88
HermannHH <harris.hermann@gmail.com>
2020-03-03 19:28:34 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Group::Member::SongRequest.delete_all
+Group::Member.delete_all
+Group.delete_all
